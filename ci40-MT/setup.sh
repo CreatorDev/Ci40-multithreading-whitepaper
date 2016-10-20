97c831b3246aaf7c336a3fdcd20460d09ae89e1a
vrakesh <rakesh.nvasudev@gmail.com>
2016-10-19 17:22:55 -0700

    Updated setup script to detect host machine arch

--- ci40-MT/setup.sh
+++ ci40-MT/setup.sh
@@ -2,12 +2,15 @@
 
 
 echo "Downloading mips linux compiler"
-
-wget http://codescape-mips-sdk.imgtec.com/components/toolchain/2016.05-03/Codescape.GNU.Tools.Package.2016.05-03.for.MIPS.MTI.Linux.CentOS-5.x86.tar.gz
-
+MACHINE_TYPE=`uname -m`
+if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+  wget http://codescape-mips-sdk.imgtec.com/components/toolchain/2016.05-03/Codescape.GNU.Tools.Package.2016.05-03.for.MIPS.MTI.Linux.CentOS-5.x86_64.tar.gz
+else
+  wget http://codescape-mips-sdk.imgtec.com/components/toolchain/2016.05-03/Codescape.GNU.Tools.Package.2016.05-03.for.MIPS.MTI.Linux.CentOS-5.x86.tar.gz
+fi
 
 
 
 echo "Extracting mips-linux-gnu-gcc"
 
-tar -xvf Codescape.GNU.Tools.Package.2016.05-03.for.MIPS.MTI.Linux.CentOS-5.x86.tar.gz 
+tar -xvf Codescape.GNU.Tools.Package.*.tar.gz 
